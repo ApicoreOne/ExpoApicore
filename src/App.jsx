f580d00aa4984d8d4e32f71a89baf6647373278d
Руслан <rd@ilab.kz>
2025-04-17 14:53:20 +0500

    feat: баннер

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import Header from "@/components/Header/Header.jsx";
 import './styles/general.scss'
 import './styles/normalize.css'
@@ -6,8 +6,38 @@ import Banner from "@/components/Banner/Banner.jsx";
 import Exponent from "@/components/Exponent/Exponent.jsx";
 import MultiModal from "@/components/MultiModal/MultiModal.jsx";
 import Overlay from "@/components/Overlay/Overlay.jsx";
+import {useEffect} from "react";
 
 function App() {
+
+  const modals = useSelector(state => state.multiModal.modals)
+  const dispatch = useDispatch()
+
+  // Закрытие модального окна по нажатию на Escape
+  const handleKeyDown = (event) => {
+
+    if (event.key === 'Escape') {
+      // Проходимся по массиву c конца , и если модальное окно открыто , то закрываем его и выходим с цикла
+      // Метод find() находит первый открытый модал.
+      const openModal = modals.slice().reverse().find(modal => modal.modalIsOpen === true);
+
+      if (openModal) {
+
+        dispatch({ type: "CLOSE_MODAL", modalLevel: openModal.modalLevel });
+      }
+
+      dispatch({type: "SWITCH_POPUP", popupType: null, popupIsOpen: false, currentCatalogItem: null})
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [modals]);
+
   return (
     <div className={'apicore-expo'}>
       <MultiModal />
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,72 @@
-import BannerTest from '@/images/banner_test.jpg'
-import styles from './Banner.module.scss'
+import { useEffect, useRef, useState } from 'react';
+import * as THREE from 'three';
+import styles from './Banner.module.scss';
+import TexpoLogo from '@/images/TexpoBanner.svg?react';
+import {useWindowWidth} from "@/hooks/index.js";
 
 const Banner = () => {
-	return(
-		<div className={styles.banner}>
+	const bannerRef = useRef(null);
+	const windowWidth = useWindowWidth();
+
+	const [vantaEffect, setVantaEffect] = useState(null);
+	const [vantaSize, setVantaSize] = useState(null);
+
+	useEffect(() => {
+		if (windowWidth <= 480) {
+			setVantaSize(6); // для самых маленьких экранов
+		} else if (windowWidth <= 530) {
+			setVantaSize(7); // для планшетов
+		} else if (windowWidth <= 1440) {
+			setVantaSize(7); // для больших экранов
+		}else if (windowWidth <= 1920) {
+			setVantaSize(9); // для экранов 2K
+		}else{
+			setVantaSize(9); // для экранов 4K
+		}
+	}, [windowWidth]);
+
+	useEffect(() => {
+		console.log('Vanta size changed:', vantaSize);
+	}, [vantaSize]);
+
+
+	useEffect(() => {
+		if (!vantaEffect) {
+			import('vanta/dist/vanta.halo.min').then((VANTA) => {
+				setVantaEffect(
+					VANTA.default({
+						el: bannerRef.current,
+						THREE,
+						mouseControls: true,
+						touchControls: true,
+						gyroControls: false,
+						baseColor: 0x742edc, // Фиолетовый цвет свечения
+						backgroundColor: 0x131a4, // Чёрный фон
+						amplitudeFactor: 1.5,
+						size: vantaSize, // Увеличиваем размер шара
+						xOffset: 0,
+						yOffset: 0,
+						speed: 1.0,
+						halo: true, // Добавляем эффект свечения
+						glowing: true, // Включаем свечение
+						color: 0x742edc, // Цвет свечения (фиолетовый)
+					})
+				);
+			});
+		}
+
+		return () => {
+			if (vantaEffect) vantaEffect.destroy();
+		};
+	}, [vantaEffect, vantaSize]);
+
+	return (
+		<div ref={bannerRef} className={styles.banner}>
 			<div className={styles.bannerImg}>
-				<img src={`${BannerTest}`} alt=""/>
+				<TexpoLogo />
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Banner
+export default Banner;
