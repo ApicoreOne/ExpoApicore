e2da0cd09144891310c4070560335221df97f7f6
Руслан <rd@ilab.kz>
2025-04-19 16:34:20 +0500

    feat: добавил адаптив header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import {useEffect, useState} from 'react';
 import styles from './Header.module.scss';
 import Logo from '@/images/ApicoreExpo.svg?react';
+import LogoMobile from '@/images/logo_circle.svg?react';
 import Account from "@/components/Account/Account.jsx";
+import TexpoLogo from "@/images/TexpoBanner.svg?react";
 
 const Header = () => {
 	const [isSticky, setIsSticky] = useState(false);
@@ -20,7 +22,15 @@ const Header = () => {
 		<div className={`${styles.header}`}>
 			<div className={`${styles.headerContent} ${isSticky ? styles.sticky : ''}`}>
 				<div className={styles.headerLogo}>
-					<Logo/>
+					<div className={styles.headerLogoDesktop}>
+						<Logo/>
+					</div>
+					<div className={styles.headerLogoMobile}>
+						<LogoMobile />
+					</div>
+				</div>
+				<div className={styles.headerLogoExpo}>
+					<TexpoLogo />
 				</div>
 				<div className={styles.headerAccount}>
 					<Account/>
